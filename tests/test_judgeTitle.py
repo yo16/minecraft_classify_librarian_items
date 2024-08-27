2993b455096648fdb419ab944b80aafb05b851f4
yo16 <xxyo1xx@gmail.com>
2024-08-27 11:40:07 +0900

    Changed comment

---
+++ tests/test_judgeTitle.py
@@ -0,0 +1,35 @@
+import pytest
+import cv2
+import json
+import os
+import numpy as np
+
+from src.readEnchantTitle import img_preprocess, get_and_erase_level
+from src.judgeTitle import judge_title
+
+
+# テストデータの整理
+json_path = "./sample-data/data_map.json"
+with open(json_path, mode="r", encoding="utf-8") as f:
+    data_map = json.load(f)
+test_params = []
+for data_info in data_map["data"]:
+    if len(data_info["file_name"]) == 0:
+        continue
+
+    base_name, ext = os.path.splitext(data_info["file_name"])
+    test_params.append((
+        f"./sample-data/{base_name}_text{ext}",
+        data_info["enchant"]
+    ))
+
+
+@pytest.mark.parametrize("img_path, expected", test_params)
+def test_classify_items(img_path, expected):
+    img = cv2.imread(img_path)
+    img_line = img.flatten()
+
+    enchant_title = judge_title(img_line)
+
+    assert enchant_title == expected
+
