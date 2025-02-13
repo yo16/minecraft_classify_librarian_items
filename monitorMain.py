4d661f02f398d7944a5f2716e820e167d41363b8
yo16 <xxyo1xx@gmail.com>
2025-02-13 22:24:05 +0900

    changed version

--- monitorMain.py
+++ monitorMain.py
@@ -1,7 +1,7 @@
 from src.monitorMinecraft import monitor_minecraft
 
 
-window_title = "Minecraft 1.21.3 - シングルプレイ"
+window_title = "Minecraft 1.21.4 - シングルプレイ"
 sleep_time = 0.2
 max_count = int(60 / sleep_time)     # 合計秒数/slee_time
 
